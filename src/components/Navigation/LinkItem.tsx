a93b005397e9bc031000e98586832e19bdfaabb0
Philipp Trentmann <philipp@bluegardenproject.com>
2024-09-28 00:26:21 +0200

    fix: fix Navigation menu issue not closing menu after click

--- src/components/Navigation/LinkItem.tsx
+++ src/components/Navigation/LinkItem.tsx
@@ -1,35 +1,47 @@
+"use client"
+
+import * as React from "react"
+import { Link as NavigationMenuLink } from "@radix-ui/react-navigation-menu"
 import { cn } from "@/utils/cn"
 import { Link } from "../Link/Link"
+import { usePathname } from "next/navigation"
 
 interface LinkItemProps {
   title: string
   description?: string
   href: string
-  className?: string
 }
 
-export function LinkItem({
-  title,
-  href,
-  description,
-  className,
-}: LinkItemProps) {
+export function LinkItem({ title, href, description }: LinkItemProps) {
+  const pathname = usePathname()
+  const isActive = href === pathname
+
   return (
-    <Link
-      href={href}
-      className={cn(
-        "hover:bg-slate-200 dark:hover:bg-slate-800 focus:shadow-custom-500 focus:shadow-[0_0_0_2px]",
-        "select-none rounded-md p-2 no-underline outline-none transition-colors",
-        "hover:scale-[102%]",
-        className
-      )}
-    >
-      <p className="font-semibold text-text-light dark:text-text-dark mb-1">
-        {title}
-      </p>
-      <p className="font-normal text-sm text-slate-500 dark:text-text-dark">
-        {description}
-      </p>
-    </Link>
+    <li>
+      <Link legacyBehavior passHref href={href}>
+        <NavigationMenuLink
+          // asChild
+          active={isActive}
+          className={cn(
+            "flex flex-col",
+            "hover:bg-slate-200 dark:hover:bg-slate-800 focus:shadow-custom-500 focus:shadow-[0_0_0_2px]",
+            "select-none rounded-md p-2 no-underline outline-none transition-colors",
+            "hover:scale-[102%]",
+            {
+              "bg-custom-100 dark:bg-custom-700": isActive,
+            }
+          )}
+        >
+          <>
+            <p className="font-semibold text-text-light dark:text-text-dark mb-1">
+              {title}
+            </p>
+            <p className="font-normal text-sm text-slate-500 dark:text-text-dark">
+              {description}
+            </p>
+          </>
+        </NavigationMenuLink>
+      </Link>
+    </li>
   )
 }
--- src/components/Navigation/UtilityContent.tsx
+++ src/components/Navigation/UtilityContent.tsx
@@ -1,4 +1,4 @@
-import { Content } from "@radix-ui/react-navigation-menu"
+import { Content, NavigationMenuLink } from "@radix-ui/react-navigation-menu"
 import { cn } from "@/utils/cn"
 import { Link } from "../Link/Link"
 import { LinkItem } from "./LinkItem"
@@ -8,22 +8,25 @@ export function UtilityContent() {
     <Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto">
       <ul className="one m-0 grid list-none gap-4 p-4 sm:w-[500px] sm:grid-cols-[0.75fr_1fr] bg-slate-50 dark:bg-slate-700">
         <li className="row-span-3 grid">
-          <Link
-            href="/"
-            className={cn(
-              "rainbow-circle",
-              "flex flex-col justify-end  h-full w-full  rounded-md p-4",
-              "focus:shadow-custom-500 dark:focus:shadow-custom-700 select-none no-underline outline-none focus:shadow-[0_0_0_2px]",
-              "hover:scale-[102%]"
-            )}
-          >
-            <div className="my-4 text-3xl font-normal text-text-inverse-light">
-              <span className="font-bold">Color</span>Finder
-            </div>
-            <p className="font-medium text-text-inverse-light text-md">
-              Tools created to enhance accessibility and efficiency for better
-              designs.
-            </p>
+          <Link legacyBehavior passHref href="/">
+            <NavigationMenuLink
+              className={cn(
+                "rainbow-circle",
+                "flex flex-col justify-end  h-full w-full  rounded-md p-4",
+                "focus:shadow-custom-500 dark:focus:shadow-custom-700 select-none no-underline outline-none focus:shadow-[0_0_0_2px]",
+                "hover:scale-[102%]"
+              )}
+            >
+              <>
+                <div className="my-4 text-3xl font-normal text-text-inverse-light">
+                  <span className="font-bold">Color</span>Finder
+                </div>
+                <p className="font-medium text-text-inverse-light text-md">
+                  Tools created to enhance accessibility and efficiency for
+                  better designs.
+                </p>
+              </>
+            </NavigationMenuLink>
           </Link>
         </li>
 
